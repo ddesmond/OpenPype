edc679d8aa6ea3b60797ced8a5f5ac65974ac9da
Roy Nieterau <roy_nieterau@hotmail.com>
2022-10-11 14:54:55 +0200

    Add example loader

---
+++ openpype/hosts/clarisse/plugins/load/load_reference.py
@@ -0,0 +1,63 @@
+from openpype.pipeline import (
+    load,
+    get_representation_path
+)
+from openpype.hosts.clarisse.api.pipeline import imprint_container
+
+
+import ix
+
+
+class ReferenceLoader(load.LoaderPlugin):
+    """Reference content into Clarisse"""
+
+    label = "Reference File"
+    families = ["*"]
+    representations = ["abc", "usd", "usda"]
+    order = 0
+
+    icon = "code-fork"
+    color = "orange"
+
+    def load(self, context, name=None, namespace=None, data=None):
+        # todo: clarisse ix batch commands?
+
+        filepath = self.fname
+
+        # Command fails on unicode so we must force it to be strings
+        # todo: can we do a better conversion, e.g. f.decode("utf8")
+        filepath = str(filepath)
+
+        # Create the file reference
+        scene_context = "project://scene"
+
+        node = ix.cmds.CreateFileReference(scene_context, [filepath])
+
+        # Imprint it with some data so ls() can find this
+        # particular loaded content and can return it as a
+        # valid container
+        imprint_container(
+            node,
+            name=name,
+            namespace=namespace,
+            context=context,
+            loader=self.__class__.__name__
+        )
+
+    def update(self, container, representation):
+        node = container["node"]
+        filepath = get_representation_path(representation)
+
+        # Command fails on unicode so we must force it to be strings
+        # todo: can we do a better conversion, e.g. f.decode("utf8")
+        filepath = str(filepath)
+        ix.cmds.SetReferenceFilename([node], filepath)
+
+        # todo: do we need to explicitly trigger reload?
+        # Update the representation id
+        ix.cmds.SetValue("{}.representation[0]".format(node),
+                         str(representation["_id"]))
+
+    def remove(self, container):
+        node = container["node"]
+        ix.cmds.DeleteItems([node.get_full_name()])
