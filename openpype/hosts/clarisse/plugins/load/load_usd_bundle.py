02a0426df946ff2134e364dc36ff2e0de411415a
Michael reda <eng.michaelreda@gmail.com>
2023-01-03 17:17:20 +0200

    add abc and usd bundle loaders after splitting into 2 separates files

---
+++ openpype/hosts/clarisse/plugins/load/load_usd_bundle.py
@@ -0,0 +1,64 @@
+import os.path
+
+from openpype.pipeline import (
+    load,
+    get_representation_path
+)
+from openpype.hosts.clarisse.api.pipeline import imprint_container
+from openpype.hosts.clarisse.api.lib import get_imports_context, create_import_contexts
+
+import ix
+
+
+class USDBundleLoader(load.LoaderPlugin):
+    """Reference USD into Clarisse USDBundle"""
+
+    label = "USD Bundle File"
+    families = ["*"]
+    representations = ["usd", "usda", "usdc"]
+    order = 0
+
+    icon = "code-fork"
+    color = "orange"
+
+    def load(self, context, name=None, namespace=None, data=None):
+        filepath = str(self.fname)
+
+        # Create the file reference
+        imports_context = str(get_imports_context()) + "/geometry"
+        create_sub_contexts = create_import_contexts()
+
+        node_name = "{}_{}".format(namespace, name) if namespace else name
+        namespace = namespace if namespace else context["asset"]["name"]
+
+        node = ix.cmds.CreateObject(namespace, "GeometryBundleUsd", "Global", imports_context)
+        ix.cmds.SetUsdBundleFilename([str(node) + ".filename[0]"], [str(filepath)], [0])
+
+        # Imprint it with some data so ls() can find this
+        # particular loaded content and can return it as a
+        # valid container
+        imprint_container(
+            node,
+            name=name,
+            namespace=namespace,
+            context=context,
+            loader=self.__class__.__name__
+        )
+        ix.application.check_for_events()
+
+    def update(self, container, representation):
+        node = container["node"]
+        filepath = get_representation_path(representation)
+        filepath = str(filepath)
+
+        ix.cmds.SetUsdBundleFilename([str(node) + ".filename[0]"], [str(filepath)], [0])
+
+        # Update the representation id
+        ix.cmds.SetValues([str("{}.openpype_representation[0]".format(node))],
+                          [str(representation["_id"])])
+        ix.application.check_for_events()
+
+    def remove(self, container):
+        node = container["node"]
+        ix.cmds.DeleteItems([node.get_full_name()])
+        ix.application.check_for_events()
